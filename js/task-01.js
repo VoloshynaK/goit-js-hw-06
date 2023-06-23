af3a38bf5130a72079c0c5f7e6a00d5bda9045b9
VoloshynaK <ko.voloshina@gmail.com>
2023-06-23 01:19:55 -0400

    new code added

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const numberOfCategories = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+numberOfCategories.forEach((item) => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const makeNewIngredient = ingredients => ingredients.map(ingredient => {
+    const indredientEl = document.createElement('li');
+    indredientEl.classList.add('item');
+    
+    indredientEl.textContent = ingredient;
+
+    return indredientEl;
+  });
+
+const elements = makeNewIngredient(ingredients);
+ingredientsListEl.append(...elements);
