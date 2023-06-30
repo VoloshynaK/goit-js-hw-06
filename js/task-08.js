2cb4d53d436cda31f388acff710df320d99eb352
VoloshynaK <ko.voloshina@gmail.com>
2023-06-30 01:23:23 -0400

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const refs = {
+    loginForm: document.querySelector('.login-form'),
+    emailInput: document.querySelector('[type="email"]'),
+    passwordInput: document.querySelector('[type="password"]'),
+    btnSubmit: document.querySelector('[type="submit"]'),
+}
 
+refs.loginForm.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (refs.emailInput.value === "" || refs.passwordInput.value === "") {
+        return alert("Please fill in all fields");
+    };
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+    refs.loginForm.reset();
+}
