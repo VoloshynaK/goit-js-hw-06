74041b728dd81a207d6166b942c824fbf4e8de33
VoloshynaK <ko.voloshina@gmail.com>
2023-06-30 01:38:23 -0400

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  colorText: document.querySelector('.color'),
+  btnChange: document.querySelector('.change-color'),
+};
+
+refs.btnChange.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  refs.colorText.textContent = getRandomHexColor();
+};
