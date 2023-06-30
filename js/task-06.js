0c050c42206bace9a9dbeebcc924e59589650387
VoloshynaK <ko.voloshina@gmail.com>
2023-06-30 00:23:00 -0400

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector('#validation-input');
+
+inputRef.addEventListener('blur', onInputChange);
+function onInputChange(event) {
+    if (event.currentTarget.value.length >= Number(event.currentTarget.dataset.length)) {
+        inputRef.classList.remove('invalid')
+        inputRef.classList.add('valid');
+    } else {
+        inputRef.classList.remove('valid');
+        inputRef.classList.add('invalid');
+    };
+    
+}
