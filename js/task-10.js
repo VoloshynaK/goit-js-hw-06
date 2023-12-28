f205b1688439373d3ec8919f1a7ad657fa64d527
VoloshynaK <ko.voloshina@gmail.com>
2023-12-28 13:41:24 -0500

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,40 @@
+const input = document.querySelector('[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => createBoxes(input.value));
+destroyBtn.addEventListener("click", destroyBoxes)
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+
+
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const color = getRandomHexColor();
+
+    console.log("hello");
+    const div = document.createElement('div');
+
+    div.style.height = `${size}px`;
+    div.style.width = `${size}px`;
+    div.style.backgroundColor = color;
+    boxes.append(div);
+
+    size += 10;
+  }
+  
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
 }
